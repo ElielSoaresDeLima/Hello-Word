9d283cfc481f2560b0548c9d93b583f018698352
ElielSoaresDeLima <140028475+ElielSoaresDeLima@users.noreply.github.com>
2024-03-18 22:51:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let nome_usuario = "";
+let elemento = document.querySelector("#nome-usuario");
+
+
+while(nome_usuario == ""){
+    nome_usuario = prompt("Qual o seu nome?");
+}
+
+if(nome_usuario == null) {
+    elemento.textContent = "Seja muito bem vindo.";
+}
+
+else{
+    elemento.textContent = nome_usuario;
+    
+}
+
